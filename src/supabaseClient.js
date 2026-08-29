d1e0edefbc93b2f52f384f332761a361f931bf88
Rasyid Hidayat <rasyidhida22gmail.com>
2026-08-29 17:22:12 +0700

    Fix conflict wedding context

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// GANTI DENGAN URL & ANON KEY MILIK ANDA DARI SUPABASE
-const supabaseUrl = 'https://snkskyrphjxqyoxcagld.supabase.co/rest/v1/'
-const supabaseAnonKey = 'sb_publishable_EItKvGaI9OBQ0LTemwKB3g_LGIY1HUV'
+const supabaseUrl = 'https://snkskyrphjxqyoxcagld.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNua3NreXJwaGp4cXlveGNhZ2xkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5OTA5ODEsImV4cCI6MjEwMzU2Njk4MX0.2AdJZ7exaVAF1ndASsVPYiMcbAvKlqQNsRXrJJrMQ5o'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
